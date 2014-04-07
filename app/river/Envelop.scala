12578e43f759d48d99e120d7ff51214fb8a1f481
Dmitry Kurinskiy <name.alari@gmail.com>
2014-04-07 18:57:03 +0400

    separate project for river core

--- app/river/Envelop.scala
+++
@@ -1,9 +0,0 @@
-package river
-
-import river.data.Event
-
-/**
- * @author alari
- * @since 4/7/14
- */
-case class Envelop(event: Event, handler: Handler)
--- app/river/Wrapper.scala
+++
@@ -1,9 +0,0 @@
-package river
-
-import river.data.Event
-
-/**
- * @author alari
- * @since 4/7/14
- */
-trait Wrapper extends ((Event)=>Handler)
